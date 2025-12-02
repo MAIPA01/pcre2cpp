2a0e1f9396420b2143f5bc3670421e4aaaefb035
MAIPA01 <marcelipatryk@gmail.com>
2025-12-02 22:18:18 +0100

    namespaces fixed

--- include/pcre2cpp/regex_compile_options.hpp
+++ include/pcre2cpp/regex_compile_options.hpp
@@ -14,45 +14,47 @@
 #pragma once
 #include "pcre2cpp_types.hpp"
 
-enum class regex_compile_options_bits : uint32_t {
-	NONE = 0u,											// No options set (default)
-	ANCHORED = PCRE2_ANCHORED,							// Force pattern anchoring
-	ALLOW_EMPTY_CLASS = PCRE2_ALLOW_EMPTY_CLASS,		// Allow empty classes
-	ALT_BSUX = PCRE2_ALT_BSUX,							// Alternative handling of \u, \U, and \x
-	ALT_CIRCUMFLEX = PCRE2_ALT_CIRCUMFLEX,				// Alternative handling of ^ in multiline mode
-	ALT_VERBNAMES = PCRE2_ALT_VERBNAMES,				// Process backslashes in verb names
-	AUTO_CALLOUT = PCRE2_AUTO_CALLOUT,					// Compile automatic callouts
-	CASELESS = PCRE2_CASELESS,							// Do caseless matching
-	DOLLAR_ENDONLY = PCRE2_DOLLAR_ENDONLY,				// $ not to match newline at end
-	DOTALL = PCRE2_DOTALL,								// . matches anything including NL
-	DUPNAMES = PCRE2_DUPNAMES,							// Allow duplicate names for subpatterns
-	ENDANCHORED = PCRE2_ENDANCHORED,					// Pattern can match only at end of subject
-	EXTENDED = PCRE2_EXTENDED,							// Ignore white space and # comments
-	FIRSTLINE = PCRE2_FIRSTLINE,						// Force matching to be before newline
-	LITERAL = PCRE2_LITERAL,							// Pattern characters are all literal
-	MATCH_INVALID_UTF = PCRE2_MATCH_INVALID_UTF,		// Enable support for matching invalid UTF
-	MATCH_UNSET_BACKREF = PCRE2_MATCH_UNSET_BACKREF,	// Match unset backreferences
-	MULTILINE = PCRE2_MULTILINE,						// ^ and $ match newlines within data
-	NEVER_BACKSLASH_C = PCRE2_NEVER_BACKSLASH_C,		// Lock out the use of \C in patterns
-	NEVER_UCP = PCRE2_NEVER_UCP,						// Lock out PCRE2_UCP, e.g.via(*UCP)
-	NEVER_UTF = PCRE2_NEVER_UTF,						// Lock out PCRE2_UTF, e.g.via(*UTF)
-	NO_AUTO_CAPTURE = PCRE2_NO_AUTO_CAPTURE,			// Disable numbered capturing paren - theses(named ones available)
-	NO_AUTO_POSSESS = PCRE2_NO_AUTO_POSSESS,			// Disable auto - possessification
-	NO_DOTSTAR_ANCHOR = PCRE2_NO_DOTSTAR_ANCHOR,		// Disable automatic anchoring for .*
-	NO_START_OPTIMIZE = PCRE2_NO_START_OPTIMIZE,		// Disable match - time start optimizations
-	NO_UTF_CHECK = PCRE2_NO_UTF_CHECK,					// Do not check the pattern for UTF validity (only relevant if PCRE2_UTF is set)
-	UCP = PCRE2_UCP,									// Use Unicode properties for \d, \w, etc.
-	UNGREEDY = PCRE2_UNGREEDY,							// Invert greediness of quantifiers
-	USE_OFFSET_LIMIT = PCRE2_USE_OFFSET_LIMIT,			// Enable offset limit for unanchored matching
-	UTF = PCRE2_UTF										// Treat pattern and subjects as UTF strings
-};
+namespace pcre2cpp {
+	enum class regex_compile_options_bits : uint32_t {
+		NONE = 0u,											// No options set (default)
+		ANCHORED = PCRE2_ANCHORED,							// Force pattern anchoring
+		ALLOW_EMPTY_CLASS = PCRE2_ALLOW_EMPTY_CLASS,		// Allow empty classes
+		ALT_BSUX = PCRE2_ALT_BSUX,							// Alternative handling of \u, \U, and \x
+		ALT_CIRCUMFLEX = PCRE2_ALT_CIRCUMFLEX,				// Alternative handling of ^ in multiline mode
+		ALT_VERBNAMES = PCRE2_ALT_VERBNAMES,				// Process backslashes in verb names
+		AUTO_CALLOUT = PCRE2_AUTO_CALLOUT,					// Compile automatic callouts
+		CASELESS = PCRE2_CASELESS,							// Do caseless matching
+		DOLLAR_ENDONLY = PCRE2_DOLLAR_ENDONLY,				// $ not to match newline at end
+		DOTALL = PCRE2_DOTALL,								// . matches anything including NL
+		DUPNAMES = PCRE2_DUPNAMES,							// Allow duplicate names for subpatterns
+		ENDANCHORED = PCRE2_ENDANCHORED,					// Pattern can match only at end of subject
+		EXTENDED = PCRE2_EXTENDED,							// Ignore white space and # comments
+		FIRSTLINE = PCRE2_FIRSTLINE,						// Force matching to be before newline
+		LITERAL = PCRE2_LITERAL,							// Pattern characters are all literal
+		MATCH_INVALID_UTF = PCRE2_MATCH_INVALID_UTF,		// Enable support for matching invalid UTF
+		MATCH_UNSET_BACKREF = PCRE2_MATCH_UNSET_BACKREF,	// Match unset backreferences
+		MULTILINE = PCRE2_MULTILINE,						// ^ and $ match newlines within data
+		NEVER_BACKSLASH_C = PCRE2_NEVER_BACKSLASH_C,		// Lock out the use of \C in patterns
+		NEVER_UCP = PCRE2_NEVER_UCP,						// Lock out PCRE2_UCP, e.g.via(*UCP)
+		NEVER_UTF = PCRE2_NEVER_UTF,						// Lock out PCRE2_UTF, e.g.via(*UTF)
+		NO_AUTO_CAPTURE = PCRE2_NO_AUTO_CAPTURE,			// Disable numbered capturing paren - theses(named ones available)
+		NO_AUTO_POSSESS = PCRE2_NO_AUTO_POSSESS,			// Disable auto - possessification
+		NO_DOTSTAR_ANCHOR = PCRE2_NO_DOTSTAR_ANCHOR,		// Disable automatic anchoring for .*
+		NO_START_OPTIMIZE = PCRE2_NO_START_OPTIMIZE,		// Disable match - time start optimizations
+		NO_UTF_CHECK = PCRE2_NO_UTF_CHECK,					// Do not check the pattern for UTF validity (only relevant if PCRE2_UTF is set)
+		UCP = PCRE2_UCP,									// Use Unicode properties for \d, \w, etc.
+		UNGREEDY = PCRE2_UNGREEDY,							// Invert greediness of quantifiers
+		USE_OFFSET_LIMIT = PCRE2_USE_OFFSET_LIMIT,			// Enable offset limit for unanchored matching
+		UTF = PCRE2_UTF										// Treat pattern and subjects as UTF strings
+	};
 
-using regex_compile_options = uint32_t;
+	using regex_compile_options = uint32_t;
 
-constexpr static regex_compile_options operator| (const regex_compile_options_bits& opt0, const regex_compile_options_bits& opt1) {
-	return static_cast<regex_compile_options>(static_cast<uint32_t>(opt0) | static_cast<uint32_t>(opt1));
-}
+	constexpr static regex_compile_options operator| (const regex_compile_options_bits& opt0, const regex_compile_options_bits& opt1) {
+		return static_cast<regex_compile_options>(static_cast<uint32_t>(opt0) | static_cast<uint32_t>(opt1));
+	}
 
-constexpr static regex_compile_options operator| (const regex_compile_options& opts, const regex_compile_options_bits& opt) {
-	return static_cast<regex_compile_options>(opts | static_cast<uint32_t>(opt));
+	constexpr static regex_compile_options operator| (const regex_compile_options& opts, const regex_compile_options_bits& opt) {
+		return static_cast<regex_compile_options>(opts | static_cast<uint32_t>(opt));
+	}
 }
--- include/pcre2cpp/regex_match_options.hpp
+++ include/pcre2cpp/regex_match_options.hpp
@@ -14,28 +14,30 @@
 #pragma once
 #include "pcre2cpp_types.hpp"
 
-enum class regex_match_options_bits : uint32_t {
-	NONE = 0u,														// No options set (default)
-	ANCHORED = PCRE2_ANCHORED,										// Match only at the first position
-	COPY_MATCHED_SUBJECT = PCRE2_COPY_MATCHED_SUBJECT,				// On success, make a private subject copy
-	DISABLE_RECURSELOOP_CHECK = PCRE2_DISABLE_RECURSELOOP_CHECK,	// Only useful in rare cases; use with care
-	ENDANCHORED = PCRE2_ENDANCHORED,								// Pattern can match only at end of subject
-	NOTBOL = PCRE2_NOTBOL,											// Subject string is not the beginning of a line
-	NOTEOL = PCRE2_NOTEOL,											// Subject string is not the end of a line
-	NOTEMPTY = PCRE2_NOTEMPTY,										// An empty string is not a valid match
-	NOTEMPTY_ATSTART = PCRE2_NOTEMPTY_ATSTART,						// An empty string at the start of the subject is not a valid match
-	NO_JIT = PCRE2_NO_JIT,											// Do not use JIT matching
-	NO_UTF_CHECK = PCRE2_NO_UTF_CHECK,								// Do not check the subject for UTF validity(only relevant if PCRE2_UTF was set at compile time)
-	PARTIAL_HARD = PCRE2_PARTIAL_HARD,								// Return PCRE2_ERROR_PARTIAL for a partial match even if there is a full match
-	PARTIAL_SOFT = PCRE2_PARTIAL_SOFT								// Return PCRE2_ERROR_PARTIAL for a partial match if no full matches are found
-};
+namespace pcre2cpp {
+	enum class regex_match_options_bits : uint32_t {
+		NONE = 0u,														// No options set (default)
+		ANCHORED = PCRE2_ANCHORED,										// Match only at the first position
+		COPY_MATCHED_SUBJECT = PCRE2_COPY_MATCHED_SUBJECT,				// On success, make a private subject copy
+		DISABLE_RECURSELOOP_CHECK = PCRE2_DISABLE_RECURSELOOP_CHECK,	// Only useful in rare cases; use with care
+		ENDANCHORED = PCRE2_ENDANCHORED,								// Pattern can match only at end of subject
+		NOTBOL = PCRE2_NOTBOL,											// Subject string is not the beginning of a line
+		NOTEOL = PCRE2_NOTEOL,											// Subject string is not the end of a line
+		NOTEMPTY = PCRE2_NOTEMPTY,										// An empty string is not a valid match
+		NOTEMPTY_ATSTART = PCRE2_NOTEMPTY_ATSTART,						// An empty string at the start of the subject is not a valid match
+		NO_JIT = PCRE2_NO_JIT,											// Do not use JIT matching
+		NO_UTF_CHECK = PCRE2_NO_UTF_CHECK,								// Do not check the subject for UTF validity(only relevant if PCRE2_UTF was set at compile time)
+		PARTIAL_HARD = PCRE2_PARTIAL_HARD,								// Return PCRE2_ERROR_PARTIAL for a partial match even if there is a full match
+		PARTIAL_SOFT = PCRE2_PARTIAL_SOFT								// Return PCRE2_ERROR_PARTIAL for a partial match if no full matches are found
+	};
 
-using regex_match_options = uint32_t;
+	using regex_match_options = uint32_t;
 
-constexpr static regex_match_options operator| (const regex_match_options_bits& opt0, const regex_match_options_bits& opt1) {
-	return static_cast<regex_match_options>(static_cast<uint32_t>(opt0) | static_cast<uint32_t>(opt1));
-}
+	constexpr static regex_match_options operator| (const regex_match_options_bits& opt0, const regex_match_options_bits& opt1) {
+		return static_cast<regex_match_options>(static_cast<uint32_t>(opt0) | static_cast<uint32_t>(opt1));
+	}
 
-constexpr static regex_match_options operator| (const regex_match_options& opts, const regex_match_options_bits& opt) {
-	return static_cast<regex_match_options>(opts | static_cast<uint32_t>(opt));
+	constexpr static regex_match_options operator| (const regex_match_options& opts, const regex_match_options_bits& opt) {
+		return static_cast<regex_match_options>(opts | static_cast<uint32_t>(opt));
+	}
 }
